abe6bda976c54a1f4a2f60e86d949522834ac1aa
SivaPrakashNiagara <striker.niagara@gmail.com>
2023-08-23 18:46:58 +0530

    inprogress of create account

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:siva_oro_2024/screens/create_account.dart';
 import 'package:siva_oro_2024/screens/splash_screen.dart';
+import 'package:siva_oro_2024/utils/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,18 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      // theme: myTheme,
       home: CreateAccountScreen(),
     );
   }
--- lib/screens/create_account.dart
+++ lib/screens/create_account.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:siva_oro_2024/custom_widgets/AppBarNeeds.dart';
 import 'package:siva_oro_2024/theme/Theme.dart';
+import 'package:csc_picker/csc_picker.dart';
 
 class CreateAccountScreen extends StatefulWidget {
   const CreateAccountScreen({super.key});
@@ -17,6 +19,216 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
         backgroundColor: PrimaryColor,
         leading: AppBarArrow(),
         title: AppBarText(title: 'Create Account',),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    child: Text('Customer Name', style: inputsHeading,),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                          )
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 10.0,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    child: Text('Phone Number', style: inputsHeading,),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: IntlPhoneField(
+                      decoration: InputDecoration(
+                        counterText: '',
+                        border: OutlineInputBorder()
+                      ),
+                    )
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10.0),
+                child: Text('Country, state & city', style: inputsHeading,),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(width: 0.5), top: BorderSide(width: 0.5), right: BorderSide(width: 0.5), left: BorderSide(width: 0.5)),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5.0)
+                ),
+                child: CSCPicker(
+                  disabledDropdownDecoration: BoxDecoration(
+                    color: liteBlue,
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                  dropdownDecoration: BoxDecoration(),
+                  dropdownHeadingStyle: inputsHeading,
+                  onCountryChanged: (value) {
+                    setState(() {
+                      // countryValue = value;
+                    });
+                  },
+                  onStateChanged:(value) {
+                    setState(() {
+                      // stateValue = value;
+                    });
+                  },
+                  onCityChanged:(value) {
+                    setState(() {
+                      // cityValue = value;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 10.0),
+                            child: Text('Pincode', style: inputsHeading,),
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: double.infinity,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                  )
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10.0,),
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 10.0),
+                            child: Text('Taluk', style: inputsHeading,),
+                          ),
+                          Container(
+                            // height: 40,
+                            color: Colors.white,
+                            width: double.infinity,
+                            child: TextFormField(
+                              textAlignVertical: TextAlignVertical.center,
+                              // style: TextStyle(fontSize: 14),
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                  )
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 10.0,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    child: Text('Village', style: inputsHeading,),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                          )
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 10.0,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    child: Text('Address 1', style: inputsHeading,),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                          )
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 10.0,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    child: Text('Address 2', style: inputsHeading,),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                          )
+                      ),
+                    ),
+                  )
+                ],
+              ),
+
+
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
