7d2d5dc7df4da60245bc797418359b74cf1ab0aa
Saravanan86810 <saravanan.niagara@gmail.com>
2023-08-23 16:13:27 +0530

    error cleared

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/Screens/home_screen.dart';
+
+import 'home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
