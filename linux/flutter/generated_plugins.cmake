4a6c0f9bba2e1cc9695459c4567311c9ece876a3
Niagara2024 <niagarakamaraj@gmail.com>
2023-08-25 10:34:46 +0530

    updated flutter vrs

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,11 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-<<<<<<< HEAD
-  file_selector_linux
-=======
   devicelocale
->>>>>>> 86810
+  file_selector_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,21 +5,15 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
-import file_selector_macos
-=======
 import devicelocale
->>>>>>> 86810
+import file_selector_macos
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-<<<<<<< HEAD
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-=======
   DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
->>>>>>> 86810
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
