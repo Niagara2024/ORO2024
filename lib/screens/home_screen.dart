302536d3942e06642435ecc81bb353f20688f99f
venkatesanios <venkatesan.niagara@gmail.com>
2023-08-26 10:40:26 +0530

    homescreen add service request

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:oro_2024/screens/AlarmList.dart';
 import 'package:oro_2024/screens/AppInfo.dart';
+import 'package:oro_2024/screens/servicerequstlist_view.dart';
 
 import 'MyPrefrence.dart';
 
@@ -193,7 +194,9 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                             onTap: () {
                               //Navigator.of(context).pop();
-                              //Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => const MyPreference()));
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      ServiceRequestlistView()));
                             },
                           ),
                         );
